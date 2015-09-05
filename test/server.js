28cf85061612f75e53dd438367ca9e898a438c25
zoltantarcsay <tarcsay.zoltan@gmail.com>
2015-09-05 14:54:14 +0100

    authenticate, authorize

--- test/server.js
+++ test/server.js
@@ -1,14 +1,35 @@
 var express = require('express'),
+    session = require('express-session'),
     passport = require('passport'),
-    openam = require('..');
+    OpenAMStrategy = require('..').OpenAMStrategy;
 
 var app = express();
 
-//app.use(openam);
+passport.use(new OpenAMStrategy({
+    serverUrl: 'http://u14.example.com:8080/openam',
+    username: 'passport',
+    password: 'changeit',
+    applicationName: 'passport'
+}));
+
+//app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize({session: false}));
+//app.use(passport.session());
+
+app.use(passport.authenticate('openam', {
+    session: false,
+    authorize: true
+    //,noRedirect: true
+}));
+
 app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello World!</h1></body></html>')
+    res.send('<html><body><h1>Hello ' + req.user + '!</h1></body></html>')
 });
 
-var server = app.listen(8080, function() {
+app.get('/foo', function (req, res) {
+    res.send('<html><body><h1>Foo bar ' + req.user + '!</h1></body></html>')
+});
+
+var server = app.listen(8080, function () {
     console.log('Server started on port %d', server.address().port);
 });
