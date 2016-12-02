9beae2a906509c9a4ec6f5c57c25b76b5c2d0f61
Zoltan Tarcsay <zoltan.tarcsay@forgerock.com>
2016-12-02 14:58:11 +0000

    resolves #10 and changes the signature of Shield.prototype.evaluate to remove the buggy Shield.prototype.init method

--- test/shield.js
+++ test/shield.js
@@ -1,4 +1,5 @@
-var should = require('should'),
+var assert = require('assert'),
+    Promise = require('bluebird'),
     sinon = require('sinon'),
     Promise = require('bluebird'),
     util = require('util'),
@@ -19,112 +20,103 @@ var mockAgent = {
     logger: console
 };
 
-console.info = console.silly = function () {};
+console.info = console.silly = function () {
+};
 
 describe('CookieShield', function () {
-    it('should set this.agent on init', function () {
-        var cookieShield = new openamAgent.CookieShield();
-        cookieShield.init(mockAgent);
-        cookieShield.agent.should.be.equal(mockAgent);
-    });
-
     it('should set params when present', function () {
         var cookieShield = new openamAgent.CookieShield({
             getProfiles: true,
             noRedirect: true
         });
-        cookieShield.getProfiles.should.be.equal(true);
-        cookieShield.noRedirect.should.be.equal(true);
+        assert(cookieShield.getProfiles);
+        assert(cookieShield.noRedirect);
     });
 
     describe('.evaluate()', function () {
         it('should call success if the session is valid', function () {
             var cookieShield = new openamAgent.CookieShield(),
-                success = sinon.spy(),
-                fail = sinon.spy(),
-                req = {headers: {cookie: 'testCookie=testSession'}};
+                req = {headers: {cookie: 'testCookie=testSession'}},
+                res = {};
 
-            cookieShield.init(mockAgent);
-            return cookieShield.evaluate(req, success, fail).then(function () {
-                success.calledOnce.should.be.equal(true);
+            return cookieShield.evaluate(req, res, mockAgent).then(function (session) {
+                assert.strictEqual(session.key, 'testSession');
             });
 
         });
 
         it('should not fail if the session is valid', function () {
             var cookieShield = new openamAgent.CookieShield(),
-                success = sinon.spy(),
-                fail = sinon.spy(),
-                req = {headers: {cookie: 'testCookie=testSession'}};
+                req = {headers: {cookie: 'testCookie=testSession'}},
+                res = {};
 
-            cookieShield.init(mockAgent);
-            return cookieShield.evaluate(req, success, fail).then(function () {
-                fail.called.should.be.equal(false);
-            });
+            return cookieShield.evaluate(req, res, mockAgent)
+                .then(function () {
+                    throw 'should not have succeeded!';
+                })
+                .catch(function (err) {
+                    assert(!!err);
+                });
         });
 
         it('should send a redirect to the proper URL if the session is invalid', function () {
             var cookieShield = new openamAgent.CookieShield(),
-                success = sinon.spy(),
-                fail = sinon.spy(),
                 req = {
                     headers: {
-                        cookie: 'testCookie=invalidSession'
-                    },
-                    res: {
-                        redirect: sinon.spy()
-                    },
-                    get: function (key) {
-                        var props = {host: 'app.example.com'};
-                        return props[key];
+                        cookie: 'testCookie=invalidSession',
+                        host: 'app.example.com'
                     }
-                };
+                },
+                res = {
+                    writeHead: sinon.spy(),
+                    end: sinon.spy()
+                },
+                success = sinon.spy(),
+                error = sinon.spy();
 
-            cookieShield.init(mockAgent);
-            return cookieShield.evaluate(req, success, fail).then(function () {
-                req.res.redirect.called.should.be.equal(true);
-                req.res.redirect.args[0][0].should.be.equal('test-login-url');
+            cookieShield.evaluate(req, res, mockAgent).then(success).catch(error);
+
+            return Promise.delay(200).then(function () {
+                assert(res.writeHead.called);
+                assert.strictEqual(res.writeHead.args[0][0], 302);
+                assert.strictEqual(res.writeHead.args[0][1].Location, 'test-login-url');
+                assert(res.end.called);
+                assert(!success.called);
+                assert(!error.called);
             });
         });
 
         it('should fail with a 401 status if the session is invalid but norRedirect is true', function () {
             var cookieShield = new openamAgent.CookieShield({cookieName: 'testCookie', noRedirect: true}),
-                success = sinon.spy(),
-                fail = sinon.spy(),
                 req = {
                     headers: {
                         cookie: 'testCookie=invalidSession'
-                    },
-                    res: {
-                        redirect: sinon.spy()
                     }
-                };
+                },
+                res = {};
 
-            cookieShield.init(mockAgent);
-            return cookieShield.evaluate(req, success, fail).then(function () {
-                req.res.redirect.called.should.be.false();
-                fail.called.should.be.true();
-                fail.args[0][0].should.be.equal(401);
-            });
+            return cookieShield.evaluate(req, res, mockAgent)
+                .then(function () {
+                    throw 'should not have been called!';
+                })
+                .catch(function (err) {
+                    assert.strictEqual(err.statusCode, 401);
+                });
         });
 
         it('should succeed if the session is invalid but passThrough is true', function () {
             var cookieShield = new openamAgent.CookieShield({cookieName: 'testCookie', passThrough: true}),
-                success = sinon.spy(),
-                fail = sinon.spy(),
                 req = {
                     headers: {
                         cookie: 'testCookie=invalidSession'
-                    },
-                    res: {
-                        redirect: sinon.spy()
                     }
-                };
+                },
+                res = {};
 
-            cookieShield.init(mockAgent);
-            return cookieShield.evaluate(req, success, fail).then(function () {
-                success.calledOnce.should.be.equal(true);
-            });
+            return cookieShield.evaluate(req, res, mockAgent)
+                .then(function (session) {
+                    assert.deepEqual(session.data, {});
+                });
         });
     });
 });
