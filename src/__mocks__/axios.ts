bbc6459003a6dd23d107d237563b5583b9adcc39
Zoltan Tarcsay <zoltan.tarcsay@forgerock.com>
2020-10-06 11:46:32 +0200

    cleanup

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,7 +1,1 @@
 export { MockAxios as default } from '../testing/mock-axios';
-
-// export default {
-//   get: () => Promise.resolve({ data: {} }),
-//   post: () => Promise.resolve({ data: {} }),
-//   put: () => Promise.resolve({ data: {} })
-// };
